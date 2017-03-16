365c4091c0a8d5f2ae0bb0c60a83668b2a5bf7aa
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-03-16 16:12:32 +0800

    50页-输入和输出

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -1,9 +1,32 @@
 var fs = require('fs');
-// var dirname = fs.readdirSync(__dirname); // 同步
 
-fs.readdir('.', async) // 异步
-function async (error, file) {
-    console.log(file);
-}
-// console.log(dirname);
-console.log('test');
+fs.readdir(process.cwd(), function (error, files) {
+    console.log('');
+    if (!files.length) {
+        return console.log('\033[31m No files to show \033[39m\n');
+    }
+    console.log(files)
+    console.log('Select shich file or directory you want to see \n');
+
+    function file (i) {
+        var filename = files[i];
+        fs.stat(__dirname + '/' +filename, function (err, stat) {
+            if (stat.isDirectory) {
+                console.log(' ' +i+ '\033[36m' +filename+ '/\033[39m');
+            } else {
+                console.log(' ' +i+ '\033[90m' +filename+ '\033[39m')
+            }
+            
+            i++;
+            if (i == files.length) {
+                console.log('');
+                process.stdout.write(' \033[33mEnter your choice：\033[39m');
+                process.stdin.resume();
+            } else {
+                file(i);
+            }
+        });
+    }
+
+    file(0);
+}) // 异步
